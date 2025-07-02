c87db07c0eb666f18de2aa856a8956215485ede0
kl1986 <kelvin.g.lee@gmail.com>
2025-07-02 22:25:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.2';
+const APP_VERSION = '1.3';
 const exerciseList = document.getElementById('exercise-list');
 const addExerciseForm = document.getElementById('add-exercise-form');
 const exerciseNameInput = document.getElementById('exercise-name');
