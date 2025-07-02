59c1984c668f37d6ed5c5c4295fc4762c11902ff
kl1986 <kelvin.g.lee@gmail.com>
2025-07-02 14:07:27 +0100

    Add update check and cache refresh

--- service-worker.js
+++ service-worker.js
@@ -13,6 +13,20 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
