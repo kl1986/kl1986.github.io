bf3183d57277a7503a5e96b82e26f940f027f2f6
kl1986 <kelvin.g.lee@gmail.com>
2025-07-07 10:27:49 +0100

    changed app version num

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.12';
+const APP_VERSION = '1.13';
 const STATUS_LABELS = ['Not done', 'Failed', 'Completed'];
 const exerciseList = document.getElementById('exercise-list');
 const addExerciseForm = document.getElementById('add-exercise-form');
